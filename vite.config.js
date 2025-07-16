658433ccb5fac879d946d0d265e2627022b83634
D-studyLab <info-prog@d-studylab.jp>
2025-07-16 21:34:11 +0900

    SEO設定

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Sitemap({ hostname: 'https://d-studylab.jp' }),],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
