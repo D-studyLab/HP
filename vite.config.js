466b31d99bc4b51bb82f795dc4737074ad81f847
D-studyLab <info-prog@d-studylab.jp>
2025-09-03 09:44:21 +0900

    リダイレクト修正ver1

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,32 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import Sitemap from 'vite-plugin-sitemap'
+import fs from 'fs'
+import path from 'path'
+
+// ブログ記事のパスを動的に取得
+const postsPath = path.resolve(__dirname, 'src/blog/posts')
+const postFiles = fs.readdirSync(postsPath).filter(file => file.endsWith('.md'))
+const dynamicRoutes = postFiles.map(file => `/blog/${file.replace('.md', '')}`)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), Sitemap({ hostname: 'https://d-studylab.jp' }),],
+  plugins: [
+    vue(),
+    Sitemap({
+      hostname: 'https://d-studylab.jp',
+      dynamicRoutes: dynamicRoutes,
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
